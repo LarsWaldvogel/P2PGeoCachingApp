425dda82e0e85d7592f9cf13f70b283fb8b7c2c8
Lars Waldvogel <lars.waldvogel@stud.unibas.ch>
2021-04-26 11:25:05 +0200

    added TODOs

--- app/src/main/java/com/example/p2pgeocaching/MainActivity.kt
+++ app/src/main/java/com/example/p2pgeocaching/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.p2pgeocaching
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
+// TODO add manifest to get bluetooth permissions
+// TODO add user interface
+// TODO add bluetooth transfer function
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
