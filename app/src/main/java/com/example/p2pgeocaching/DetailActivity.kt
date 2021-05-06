08046894ff3010515d44932c058bcd24ce9eb447
Lars Waldvogel <lars.waldvogel@stud.unibas.ch>
2021-05-06 15:31:42 +0200

    added DetailActivity class

--- app/src/main/java/com/example/p2pgeocaching/DetailActivity.kt
+++ app/src/main/java/com/example/p2pgeocaching/DetailActivity.kt
@@ -1,4 +1,50 @@
 package com.example.p2pgeocaching
 
-class DetailActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.solver.Cache
+import com.example.p2pgeocaching.data.CacheData
+import com.example.p2pgeocaching.data.CacheDataParser
+import com.example.p2pgeocaching.databinding.ActivityMainBinding
+
+/**
+ * This class is used when viewing the details of a Cache
+ */
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val CACHE = "cache"
+    }
+
+    private lateinit var binding: ActivityDetailBinding // TODO create activity_detail.xml
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize binding object
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Check if a CacheData object was given
+        // If no CachedData object was given, return to previous activity
+        val cacheData = intent?.extras?.getSerializable(CACHE)
+        // TODO: cast from Serializable to CacheData
+        if (cacheData == null || cacheData !is CacheData) {
+            finish()
+        }
+
+        // If a cache was given, parse it to
+        val cache = CacheDataParser.dataToCache(cacheData)
+        // TODO: initialize the fields in the UI
+        //  Contents: Show title, desc, creator, plainTextHOF
+        //  if it is a ownCache: show privateKey when asked
+        //  if it is a unsolvedCache: show button to solve cache, which leads to own activity
+        //  if it is a solvedCache: show a "Solved!" text
+
+
+
+
+    }
+
 }
