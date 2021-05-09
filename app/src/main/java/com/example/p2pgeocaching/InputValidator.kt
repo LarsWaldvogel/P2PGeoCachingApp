0ac9947fbc8a999f5dfccc6f6e09cf8f1ab16d91
Lars Waldvogel <lars.waldvogel@stud.unibas.ch>
2021-05-09 18:17:20 +0200

    changed InputValidator so it only accepts letters, space and numbers

--- app/src/main/java/com/example/p2pgeocaching/InputValidator.kt
+++ app/src/main/java/com/example/p2pgeocaching/InputValidator.kt
@@ -7,7 +7,8 @@ class InputValidator {
     // TODO change, so it contains only a-z, A-Z, " ", 0-9
 
     companion object {
-        val illegalCharacters: List<Char> = listOf(';', '{', '}', '"')
+        private val legalCharacters: List<Char> = generateLegalCharacters()
+
 
         /**
          * Simple function that checks if any of the Strings provided in [arguments] contains an illegal
@@ -17,19 +18,54 @@ class InputValidator {
         fun checkForIllegalCharacters(arguments: List<String>) {
             // Checks for illegal characters in strings
             for (str in arguments) {
-                for (illChr in illegalCharacters) {
-                    if (str.contains(illChr)) {
+                for (char in str) {
+                    if (char !in legalCharacters) {
                         throw StringContainsIllegalCharacterException()
                     }
                 }
             }
         }
 
+
         /**
          * If only a single string is provided, casts it to array list and calls original function.
          */
         fun checkForIllegalCharacters(argument: String) {
             checkForIllegalCharacters(arrayListOf(argument))
+        }
+
+
+        /**
+         * This function generates a list of all the legal characters and returns it.
+         * The legal characters are the letters a-z, A-Z and numbers 0-9.
+         */
+        fun generateLegalCharacters(): List<Char> {
+            // Create list with space character ' ' in it
+            val listOfChars = mutableListOf(' ')
+
+            // Add characters a-z
+            var char = 'a'
+            while (char <= 'z') {
+                listOfChars.add(char)
+                char++
+            }
+
+            // Add characters A-Z
+            char = 'A'
+            while (char <= 'Z') {
+                listOfChars.add(char)
+                char++
+            }
+
+            // Add numbers 0-9
+            char = '0'
+            while (char <= '9') {
+                listOfChars.add(char)
+                char++
+            }
+
+            // Return result
+            return listOfChars
         }
     }
 }
