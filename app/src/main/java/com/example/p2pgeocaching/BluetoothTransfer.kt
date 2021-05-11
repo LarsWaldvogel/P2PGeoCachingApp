caf3f884cfd6a8b02cddce3f19125a3fbe2dc611
Kapilas Gnanarajah <kapilas.gnanarajah@stud.unibas.ch>
2021-05-11 08:19:43 +0200

    added TODO to BluetoothTransfer.kt

--- app/src/main/java/com/example/p2pgeocaching/BluetoothTransfer.kt
+++ app/src/main/java/com/example/p2pgeocaching/BluetoothTransfer.kt
@@ -4,5 +4,6 @@ package com.example.p2pgeocaching
  * Sends list of all caches to other device and receives the other device's list of caches
  * Uses byte stream to transfer JSON files of the CacheList objects
  */
+// TODO turn this class into main class for all Bluetooth Setup and communication
 class BluetoothTransfer {
 }
