19031fc1a9c2a60d1913fd375d6b8c9cfb92c08f
Kapilas Gnanarajah <kapilas.gnanarajah@stud.unibas.ch>
2021-05-19 15:55:01 +0200

    commented out the discovery function and introduced an arrayAdapter of the paired devices to choose from

--- app/src/main/java/com/example/p2pgeocaching/bluetooth/BluetoothTransfer.kt
+++ app/src/main/java/com/example/p2pgeocaching/bluetooth/BluetoothTransfer.kt
@@ -8,7 +8,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
+//import android.support.v4.app.DialogFragment
+import com.example.p2pgeocaching.R
 
 /**
  * Sends list of all caches to other device and receives the other device's list of caches
@@ -19,11 +22,13 @@ class BluetoothTransfer: Activity() {
 
     private lateinit var bluetoothAdapter: BluetoothAdapter
     var listOfDevices: Set<BluetoothDevice>? = null
+    var arrayOfDevices: ArrayAdapter<String>? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        arrayOfDevices = ArrayAdapter(this,R.layout.bluetooth_select_device)
         if(bluetoothAdapter == null) {
             println("bluetooth disabled")
         } else {
@@ -33,7 +38,7 @@ class BluetoothTransfer: Activity() {
     }
 
     // Create a BroadcastReceiver for ACTION_FOUND.
-    private val receiver = object : BroadcastReceiver() {
+/*    private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val action = intent.action
             when(action) {
@@ -52,6 +57,7 @@ class BluetoothTransfer: Activity() {
             }
         }
     }
+*/
 
     /**
      * This method discovers devices near you and lists these  in [listOfDevices]
@@ -59,7 +65,16 @@ class BluetoothTransfer: Activity() {
     private fun discoverDevices() {
         // already paired devices
         listOfDevices = bluetoothAdapter.bondedDevices
+        for (device in (listOfDevices as MutableSet<BluetoothDevice>?)!!) {
+            // Add the name and address to an array adapter to show in a ListView
+            arrayOfDevices!!.add((if (device.name != null) device.name else "Unknown") + "\n" + device.address + "\nPaired")
+        }
+        val client = ChooseClient()
+        // TODO client.show(getSupportFragmentManager(), "client_chosen")
+        //  show fragment doesn't work import android.support.v4.app.DialogFragment doesnt work
 
+
+/* uncomment if you want to discover devices from inside the app
         if (bluetoothAdapter.isDiscovering) {
             bluetoothAdapter.cancelDiscovery()
         }
@@ -68,11 +83,12 @@ class BluetoothTransfer: Activity() {
         // Register for broadcasts when a device is discovered.
         val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
         this.registerReceiver(receiver, filter)
+*/
     }
 
-    override fun onDestroy() {
+override fun onDestroy() {
         super.onDestroy()
-        bluetoothAdapter.cancelDiscovery()
-        unregisterReceiver(receiver)
+        // bluetoothAdapter.cancelDiscovery()
+        // unregisterReceiver(receiver)
     }
 }
