a7349d14c52dd5d5773accae73c4d1cbc1cf0be8
Lars Waldvogel <lars.waldvogel@stud.unibas.ch>
2021-06-27 23:36:44 +0200

    formatting

--- app/src/main/java/com/example/p2pgeocaching/caches/CacheList.kt
+++ app/src/main/java/com/example/p2pgeocaching/caches/CacheList.kt
@@ -1,6 +1,7 @@
 package com.example.p2pgeocaching.caches
 
 // TODO: list should update with bluetooth transfer
+
 /** Contains the list of caches of the device
  * Is updated upon file transfer via bluetooth
  */
---
+++ app/src/main/java/com/example/p2pgeocaching/constants/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.p2pgeocaching.constants
+
+class Constants {
+}
