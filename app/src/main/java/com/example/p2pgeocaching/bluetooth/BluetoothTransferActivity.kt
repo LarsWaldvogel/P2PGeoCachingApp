dda19215f60f117bea15cc31ce6e98b4c798308a
Kapilas Gnanarajah <kapilas.gnanarajah@stud.unibas.ch>
2021-07-01 12:24:55 +0200

    This class will become the intersection between gui and datatranfer

---
+++ app/src/main/java/com/example/p2pgeocaching/bluetooth/BluetoothTransferActivity.kt
@@ -0,0 +1,19 @@
+package com.example.p2pgeocaching.bluetooth
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This activity handles all bluetooth related things
+ */
+class BluetoothTransferActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "BluetoothTransferActivity"
+    }
+
+    var bluetoothActive = false
+
+
+
+
+}
