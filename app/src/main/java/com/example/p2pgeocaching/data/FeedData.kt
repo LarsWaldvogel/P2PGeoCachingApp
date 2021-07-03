5eb039b482abd0aeaec863bd28fac0e41f343909
Kapilas Gnanarajah <kapilas.gnanarajah@stud.unibas.ch>
2021-07-03 20:17:07 +0200

    stylecheck FeedData

--- app/src/main/java/com/example/p2pgeocaching/data/FeedData.kt
+++ app/src/main/java/com/example/p2pgeocaching/data/FeedData.kt
@@ -1,9 +1,6 @@
 package com.example.p2pgeocaching.data
 
-import android.provider.SyncStateContract
 import android.util.Log
-import com.example.p2pgeocaching.activities.NewCacheActivity
-import com.example.p2pgeocaching.bluetooth.BluetoothHandler
 import com.example.p2pgeocaching.caches.CacheListGenerator
 import com.example.p2pgeocaching.constants.Constants
 import java.io.File
@@ -33,7 +30,7 @@ class FeedData : Serializable {
         const val TAG = "FeedData"
     }
 
-    fun feedToData(context: File): String {
+    private fun feedToData(context: File): String {
         val file = File(context, "file")
         if (!file.exists()) {
             file.createNewFile()
@@ -43,37 +40,37 @@ class FeedData : Serializable {
         val userNameFile = File(context, Constants.U_NAME_FILE)
         var userName = userNameFile.readLines().toString()
         userName = userName.substring(1, userName.length - 1)
-        var personData = File(context, Constants.PERSON_DATA)
+        val personData = File(context, Constants.PERSON_DATA)
         val content = personData.readText()
         val keys = content.split(" ")
         val pubkey = keys[0].split("_")
         val salt = pubkey[1].takeLast(4)
-        val feedname = userName.plus("#").plus(salt)
-        Log.i(TAG, "feedName = "+feedname)
-        val ownFeedFile = File(context, feedname)
+        val feedName = userName.plus("#").plus(salt)
+        Log.i(TAG, "feedName = $feedName")
+        val ownFeedFile = File(context, feedName)
         val ownFeedContent = ownFeedFile.readText()
         Log.i(TAG, "Before if")
-        if (ownFeedContent.length != 0) {
+        if (ownFeedContent.isNotEmpty()) {
             Log.i(TAG, "length is not zero")
-            file.appendText(feedname)
+            file.appendText(feedName)
             file.appendText("_:_:_")
             file.appendText("".plus(ownFeedContent))
             Log.i(TAG, "File Content after adding own feed = "+file.readText())
         }
 
-        var feedNamesFile = File(context, Constants.FEED_NAMES_FILE)
+        val feedNamesFile = File(context, Constants.FEED_NAMES_FILE)
         if (feedNamesFile.length() != 0L) {
-            var feedNameList = feedNamesFile.readText().split("\n")
+            val feedNameList = feedNamesFile.readText().split("\n")
 
             for (feed in feedNameList) {
                 val feedFile = File(context, feed)
                 val feedContent = feedFile.readText()
-                if (file.length() == 0L && feedContent.length != 0) {
+                if (file.length() == 0L && feedContent.isNotEmpty()) {
                     file.appendText(feed)
                     file.appendText("_:_:_")
                     file.appendText("".plus(feedContent))
                     Log.i(TAG, "File Content after adding new feed = " + file.readText())
-                } else if (feedContent.length != 0) {
+                } else if (feedContent.isNotEmpty()) {
                     file.appendText("#####")
                     file.appendText("".plus(feed))
                     file.appendText("_:_:_")
@@ -89,7 +86,7 @@ class FeedData : Serializable {
         return file.readText()
     }
 
-    fun dataToFeed (file: File, context:File) {
+    private fun dataToFeed (file: File, context:File) {
         Log.i(TAG, "started dataToFeed")
         val fileContentList = file.readText().split("$$$$$")
         val sizeOfFileContent = fileContentList.size
@@ -99,35 +96,35 @@ class FeedData : Serializable {
             Log.i(TAG, "Received File Content = $fileContent")
             val feedList = fileContent.split("#####")
 
-            var feedNamesFile = File(context, Constants.FEED_NAMES_FILE)
+            val feedNamesFile = File(context, Constants.FEED_NAMES_FILE)
             if (feedNamesFile.length() != 0L) {
-                var feedNameList = feedNamesFile.readText().split("\n")
+                val feedNameList = feedNamesFile.readText().split("\n")
 
                 val fdp = FeedDataParser()
 
                 val newEntryList = mutableListOf<Entry>()
 
                 for (feed in feedList) {
-                    Log.i(TAG, "Feed = " + feed)
+                    Log.i(TAG, "Feed = $feed")
                     val feedElem = feed.split("_:_:_")
                     val feedName = feedElem[0]
-                    Log.i(TAG, "FeedName = " + feedName)
+                    Log.i(TAG, "FeedName = $feedName")
                     val feedContent = feedElem[1]
-                    Log.i(TAG, "FeedContent = " + feedContent)
+                    Log.i(TAG, "FeedContent = $feedContent")
                     for (person in feedNameList) {
-                        if (person.equals(feedName)) {
+                        if (person == feedName) {
                             Log.i(TAG, "Feed is subscribed")
                             val personFile = File(context, person)
                             if (personFile.length() == 0L) {
                                 personFile.appendText(feedContent)
                                 val feedEntryList = fdp.feedToEntrylist(personFile)
-                                for (i in 0..(feedEntryList.size - 1)) {
-                                    newEntryList.add(feedEntryList[i])
+                                for (element in feedEntryList) {
+                                    newEntryList.add(element)
                                 }
                             } else {
-                                val entrylist = fdp.feedToEntrylist(personFile)
-                                var len = entrylist.size
-                                val lastEntry = entrylist[len - 1]
+                                val entryList = fdp.feedToEntrylist(personFile)
+                                var len = entryList.size
+                                val lastEntry = entryList[len - 1]
 
                                 val supportFile = File(context, "support")
                                 if (!supportFile.exists()) {
@@ -152,7 +149,7 @@ class FeedData : Serializable {
                                     personFile.createNewFile()
                                     personFile.appendText(supportFile.readText())
                                     Log.i(TAG, "New Feed Content = " + personFile.readText())
-                                    break;
+                                    break
                                 }
                             }
                         }
@@ -163,21 +160,21 @@ class FeedData : Serializable {
                     val userNameFile = File(context, Constants.U_NAME_FILE)
                     var userName = userNameFile.readLines().toString()
                     userName = userName.substring(1, userName.length - 1)
-                    var personData = File(context, Constants.PERSON_DATA)
+                    val personData = File(context, Constants.PERSON_DATA)
                     val content = personData.readText()
                     val keys = content.split(" ")
                     val pubkey = keys[0].split("_")
                     val salt = pubkey[1].takeLast(4)
-                    val feedname = userName.plus("#").plus(salt)
+                    val feedName = userName.plus("#").plus(salt)
 
                     val ownPublisher = OwnPublisher(userName, keys[0], keys[1])
 
-                    var ownFeedFile = File(context, feedname)
-                    var ownFeedList = fdp.feedToEntrylist(ownFeedFile)
+                    val ownFeedFile = File(context, feedName)
+                    val ownFeedList = fdp.feedToEntrylist(ownFeedFile)
 
                     val ownFeed = OwnFeed(ownFeedList, ownPublisher)
 
-                    var logEntry = LogEntry.Companion.newLogEntry(newEntryList, ownFeed, context)
+                    val logEntry = LogEntry.Companion.newLogEntry(newEntryList, ownFeed, context)
 
                     val appendtext = fdp.appendCacheToFeed(logEntry)
                     Log.i(TAG, "Append Text = " + appendtext)
